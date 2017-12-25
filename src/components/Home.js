fd3411372d28216a0b881c401bd6b128bfa52503
xx18 <xx18@global.tesco.org>
2017-12-25 17:29:01 +0530

    two way binding

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,9 +14,16 @@ export default class Home extends React.Component {
         this.props.updateStatus(this.state.status);
     }
 
+    changeInput(event){
+        this.setState({
+            status:event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
+                <input type="text" value={this.state.status} onChange={(event)=>(this.changeInput(event))}/>
                 <button onClick={this.changeStatus.bind(this)}>Home Button </button>
             </div>
         )
